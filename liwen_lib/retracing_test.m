5b0fc7c96684fb4230d635156916262182814940
Liwen Wen <wenliwen64@gmail.com>
2015-05-13 00:55:06 -0700

    correct cos -> cosd

--- liwen_lib/retracing_test.m
+++ liwen_lib/retracing_test.m
@@ -3,7 +3,7 @@ function loc_new_grid = retracing_test(loc_sta, loc_grid, depth)
      sin_takeoff_angle = ret.rayParam/(6400*2*3.1415926/360)*6.2/57.3;
      cos_takeoff_angle = sqrt(1 - sin_takeoff_angle^2);
      [dk, dd, daze, dazs] = distaz(loc_sta(1), loc_sta(2), loc_grid(1), loc_grid(2));
-     ini_vec = [1*sin_takeoff_angle*sin(daze), 1*sin_takeoff_angle*cos(daze), -1*cos_takeoff_angle];
+     ini_vec = [1*sin_takeoff_angle*sind(dazs), 1*sin_takeoff_angle*cosd(dazs), -1*cos_takeoff_angle];
      %ret2 = taupPierce('prem', depth, 'P', 'sta', loc_sta, 'evt', loc_grid);
      ini_point = [loc_grid(1), loc_grid(2), -20];
      v0_point = [loc_grid(1), loc_grid(2), -40];
@@ -14,7 +14,15 @@ function loc_new_grid = retracing_test(loc_sta, loc_grid, depth)
          moho_norm_flat_vec, third_layer_depth);
      [loc_new_grid, new_vec, I0_2] = raytracing_liwen_upward(loc_third_point, -downward_vec, v0_point, moho_norm_actual_vec, [loc_grid(1), loc_grid(2), -depth]);
      ret.rayParam
+     downward_vec
+     loc_third_point
      sin_takeoff_angle
      cos_takeoff_angle
      I0_2
+     ini_vec
+     daze
+     dazs
+     sin_takeoff_angle
+     
+     1*sin_takeoff_angle*cosd(dazs)
 end
