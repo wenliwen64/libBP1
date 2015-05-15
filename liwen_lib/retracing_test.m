7e5a364983ec79619e70c1fd881d5d84769301d4
Liwen <wenliwen64@gmail.com>
2015-05-14 20:14:00 -0700

    add km2d and d2km

--- liwen_lib/retracing_test.m
+++ liwen_lib/retracing_test.m
@@ -1,6 +1,6 @@
 %retracing test ...
 
-load /home/lwen/Documents/seis_research/raw_data/Hinet_nepal.v2.mat
+%load /home/lwen/Documents/seis_research/raw_data/Hinet_nepal.v2.mat
 
 ep_lat = ret.lat0;
 ep_lon = ret.lon0;
@@ -12,8 +12,10 @@ evt_depth = 20;
 loc_new_grid = zeros(grid_dim(1), grid_dim(2));
 count = 1;
 ii = 100;
-for i = 1:40
-    for j = 1:40
+
+new_grid = zeros(40, 40)
+for i = 1:40 % latitude
+    for j = 1:40 % longitude
         loc_grid = [-2+i*lat_step+ep_lat, -2+j*lon_step+ep_lon];
         loc_sta = [ret.lat(ii), ret.lon(ii)];
         loc_new_grid = retracing_liwen(loc_sta, loc_grid, evt_depth);
@@ -21,7 +23,13 @@ for i = 1:40
         loc_new_grid_x(count) = loc_new_grid(2);
         loc_old_grid_x(count) = -2+j*lon_step+ep_lon;
         loc_old_grid_y(count) = -2+i*lat_step+ep_lat;
+        
+        new_grid_x(i, j) = loc_new_grid_x(count);
+        new_grid_y(i, j) = loc_new_grid_y(count);
+        new_grid_timeshift(i, j) = timeshift;        
         count = count+1;
+
+        
     end
 end
 
